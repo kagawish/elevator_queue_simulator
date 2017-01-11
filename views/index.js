6ead4ec3efae34e2fd13643339dd7d6b07a5e352
kagawish <karimgawish.1@gmail.com>
2017-01-11 08:53:45 +0200

    [Remove] Commenting ReactJS until figuring out whats the problem.

--- views/index.js
+++ views/index.js
@@ -1,50 +1,50 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
-import injectTapEventPlugin from 'react-tap-event-plugin';
-import MyAwesomeReactComponent from './MyAwesomeReactComponent';
+// import React from 'react';
+// import ReactDOM from 'react-dom';
+// import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
+// import injectTapEventPlugin from 'react-tap-event-plugin';
+// import MyAwesomeReactComponent from './MyAwesomeReactComponent';
 
-injectTapEventPlugin();
+// injectTapEventPlugin();
 
-const App = () => (
-  // <MuiThemeProvider>
-  //   <MyAwesomeReactComponent />
-  // </MuiThemeProvider>
-  <h1>
-  	Hello World!
-  </h1>
-);
+// const App = () => (
+//   <MuiThemeProvider>
+//     <MyAwesomeReactComponent />
+//   </MuiThemeProvider>
+//   <h1>
+//   	Hello World!
+//   </h1>
+// );
 
-ReactDOM.render(
-  <App />,
-  document.getElementById('app')
-);
+// ReactDOM.render(
+//   <App />,
+//   document.getElementById('app')
+// );
 
 
-// var myElevatorQueuingSystem = new QueuingSystem('MyElevatorQueueSystem');
+var myElevatorQueuingSystem = new QueuingSystem('MyElevatorQueueSystem');
 
-// var myElevator = new Elevator('MyElevator', 400);
+var myElevator = new Elevator('MyElevator', 400);
 
-// var myMaterialA = new Material('MyMaterialA', 200, 'uniform', 5, 2);
-// var myMaterialB = new Material('MyMaterialB', 100, 'constant', 6);
-// var myMaterialC = new Material('MyMaterialC', 50, 'probabilistic', [2, 3], [0.33, 0.67]);
+var myMaterialA = new Material('MyMaterialA', 200, 'uniform', 5, 2);
+var myMaterialB = new Material('MyMaterialB', 100, 'constant', 6);
+var myMaterialC = new Material('MyMaterialC', 50, 'probabilistic', [2, 3], [0.33, 0.67]);
 
-// var myQueue = new Queue('MyQueue');
+var myQueue = new Queue('MyQueue');
 
-// myElevatorQueuingSystem._total_time = 200;
+myElevatorQueuingSystem._total_time = 200;
 
-// myElevatorQueuingSystem.assign_elevator(myElevator);
+myElevatorQueuingSystem.assign_elevator(myElevator);
 
-// myElevatorQueuingSystem.assign_material(myMaterialA);
-// myElevatorQueuingSystem.assign_material(myMaterialB);
-// myElevatorQueuingSystem.assign_material(myMaterialC);
+myElevatorQueuingSystem.assign_material(myMaterialA);
+myElevatorQueuingSystem.assign_material(myMaterialB);
+myElevatorQueuingSystem.assign_material(myMaterialC);
 
-// myElevatorQueuingSystem.link_queue(myQueue);
+myElevatorQueuingSystem.link_queue(myQueue);
 
-// myElevator.link_system(myElevatorQueuingSystem);
+myElevator.link_system(myElevatorQueuingSystem);
 
-// console.log(myElevatorQueuingSystem);
+console.log(myElevatorQueuingSystem);
 
-// myElevatorQueuingSystem.advance_timeline(1);
+myElevatorQueuingSystem.advance_timeline(1);
 
 
