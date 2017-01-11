ec3dd19c0e3a7988186477fec9ed77c58ad8d710
kagawish <karimgawish.1@gmail.com>
2017-01-11 08:51:50 +0200

    [Add] Continuing configuring React (But not working yet).

--- views/index.js
+++ views/index.js
@@ -1,30 +1,50 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
+import injectTapEventPlugin from 'react-tap-event-plugin';
+import MyAwesomeReactComponent from './MyAwesomeReactComponent';
 
-var myElevatorQueuingSystem = new QueuingSystem('MyElevatorQueueSystem');
+injectTapEventPlugin();
 
-var myElevator = new Elevator('MyElevator', 400);
+const App = () => (
+  // <MuiThemeProvider>
+  //   <MyAwesomeReactComponent />
+  // </MuiThemeProvider>
+  <h1>
+  	Hello World!
+  </h1>
+);
 
-var myMaterialA = new Material('MyMaterialA', 200, 'uniform', 5, 2);
-var myMaterialB = new Material('MyMaterialB', 100, 'constant', 6);
-var myMaterialC = new Material('MyMaterialC', 50, 'probabilistic', [2, 3], [0.33, 0.67]);
+ReactDOM.render(
+  <App />,
+  document.getElementById('app')
+);
 
-var myQueue = new Queue('MyQueue');
 
-myElevatorQueuingSystem._total_time = 200;
+// var myElevatorQueuingSystem = new QueuingSystem('MyElevatorQueueSystem');
 
-myElevatorQueuingSystem.assign_elevator(myElevator);
+// var myElevator = new Elevator('MyElevator', 400);
 
-myElevatorQueuingSystem.assign_material(myMaterialA);
-myElevatorQueuingSystem.assign_material(myMaterialB);
-myElevatorQueuingSystem.assign_material(myMaterialC);
+// var myMaterialA = new Material('MyMaterialA', 200, 'uniform', 5, 2);
+// var myMaterialB = new Material('MyMaterialB', 100, 'constant', 6);
+// var myMaterialC = new Material('MyMaterialC', 50, 'probabilistic', [2, 3], [0.33, 0.67]);
 
-myElevatorQueuingSystem.link_queue(myQueue);
+// var myQueue = new Queue('MyQueue');
 
-myElevator.link_system(myElevatorQueuingSystem);
+// myElevatorQueuingSystem._total_time = 200;
 
-console.log(myElevatorQueuingSystem);
+// myElevatorQueuingSystem.assign_elevator(myElevator);
 
-myElevatorQueuingSystem.advance_timeline(1);
+// myElevatorQueuingSystem.assign_material(myMaterialA);
+// myElevatorQueuingSystem.assign_material(myMaterialB);
+// myElevatorQueuingSystem.assign_material(myMaterialC);
+
+// myElevatorQueuingSystem.link_queue(myQueue);
+
+// myElevator.link_system(myElevatorQueuingSystem);
+
+// console.log(myElevatorQueuingSystem);
+
+// myElevatorQueuingSystem.advance_timeline(1);
 
 
